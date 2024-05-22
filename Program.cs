6529f07fe73d834263d21809910b01ef4ee9cba0
Adriano Aparecido da Silva <adriano.profile.ads.cloud@gmail.com>
2024-05-22 05:52:59 -0300

    Projeto finalizado

--- Program.cs
+++ Program.cs
@@ -3,23 +3,123 @@ using DesafioProjetoHospedagem.Models;
 
 Console.OutputEncoding = Encoding.UTF8;
 
-// Cria os modelos de hóspedes e cadastra na lista de hóspedes
-List<Pessoa> hospedes = new List<Pessoa>();
+   List<Pessoa> hospedes = new();
+   Reserva reserva = new();
+   Suite suite = new();
 
-Pessoa p1 = new Pessoa(nome: "Hóspede 1");
-Pessoa p2 = new Pessoa(nome: "Hóspede 2");
+   while(true)
+   {
+    Console.WriteLine("Selecione uma opção:");
+            Console.WriteLine("1. Cadastrar Hóspede");
+            Console.WriteLine("2. Cadastrar Suite");
+            Console.WriteLine("3. Realizar Reserva");
+            Console.WriteLine("4. Realizar Checkout");
+            Console.WriteLine("5. Sair");
 
-hospedes.Add(p1);
-hospedes.Add(p2);
+            Console.Write("Opção: ");
+            string selecaoDoMenu = Console.ReadLine();
 
-// Cria a suíte
-Suite suite = new Suite(tipoSuite: "Premium", capacidade: 2, valorDiaria: 30);
+            switch (selecaoDoMenu)
+            {
+                case "1":
+                    // Cadastrar Hóspede
+                    Console.WriteLine("Informe o nome do Hospede");
+                    string nomeHospede = Console.ReadLine();
+                    
+                    try
+                    { 
+                    // Adicionar hóspede à lista temporária 
+                         Pessoa pessoa = new Pessoa();
+                         pessoa.Nome = nomeHospede;
+                         hospedes.Add(pessoa);
+                        reserva.CadastrarHospedes(hospedes );
+                        Console.WriteLine("Hóspede cadastrado com sucesso!");
+                        Console.WriteLine($"Hóspedes: {reserva.ObterQuantidadeHospedes()}");
+                    }
+                    catch (InvalidOperationException ex)
+                    {
+                        Console.WriteLine($"Erro: {ex.Message}");
+                    }
+                    break;
+                
+                case "2":
+                    // Cadastrar Suite
+                    Console.WriteLine("Cadastrar Suite");
+                    Console.WriteLine("Por favor, informe os dados da suite a seguir:");
+                    Console.WriteLine("Informe o tipo de suite"); 
+                    string tipoSuite = Console.ReadLine();
+                    Console.WriteLine("Informe a capacidade da suite");
+                    string capacidadeSuite = Console.ReadLine();
+                    Console.WriteLine("Informe  o valor da diária:");
+                    string valorDiaria = Console.ReadLine();
+                    
+                    try
+                     {
+                     // Adicionar suite                                    
+                           suite.TipoSuite = tipoSuite;
+                           suite.Capacidade = int.Parse(capacidadeSuite);
+                           suite.ValorDiaria = decimal.Parse(valorDiaria);                   
+                                                       
+                  
+                        reserva.CadastrarSuite(suite);
+                        Console.WriteLine("Suite cadastrada com sucesso!");
+                    }
+                    catch (InvalidOperationException ex)
+                    {
+                        Console.WriteLine($"Erro: {ex.Message}");
+                    }
+                    break;
 
-// Cria uma nova reserva, passando a suíte e os hóspedes
-Reserva reserva = new Reserva(diasReservados: 5);
-reserva.CadastrarSuite(suite);
-reserva.CadastrarHospedes(hospedes);
+                case "3":
+                    // Realizar Reserva
+                    Console.WriteLine("Realizar Reserva");
+                    Console.WriteLine("Por favor, informe os dados da reserva a seguir:");
+                    Console.WriteLine("Informe a quantidade de dias a ser reservado"); 
+                    string realizarReserva = Console.ReadLine();                                 
+                                                      
+                    try
+                    {
+                        reserva.DiasReservados = int.Parse(realizarReserva);
+                        Console.WriteLine("Reserva realizada com Sucesso");
+                        Console.WriteLine($"Valor diária: {reserva.CalcularValorDiaria()}"); 
+                    }
+                    catch (InvalidOperationException ex)
+                    {
+                        Console.WriteLine($"Erro: {ex.Message}");
+                    }
+                    break;    
 
-// Exibe a quantidade de hóspedes e o valor da diária
-Console.WriteLine($"Hóspedes: {reserva.ObterQuantidadeHospedes()}");
-Console.WriteLine($"Valor diária: {reserva.CalcularValorDiaria()}");
+                    case "4":
+                    // Realizar Checkout
+                   
+                    foreach(Pessoa hop in hospedes )
+                    {
+                       
+                        Console.WriteLine(hop.Nome);
+                    }
+                     Console.WriteLine("Selecione o Hospede");
+                     string hospSelecinado = Console.ReadLine();
+                       hospedes.RemoveAll(h => h.Nome == hospSelecinado);
+                       Console.WriteLine("Checkout realizado com sucesso!");
+                    break;    
+
+                case "5":
+                    // Sair
+                    Console.WriteLine("Saindo...");
+                    return; // Sai do programa
+                
+                default:
+                    Console.WriteLine("Opção inválida. Tente novamente.");
+                    break;
+             
+            }
+            
+         
+        }
+        
+
+ 
+
+
+
+
